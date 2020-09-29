ed68d4a52830e6ceb6c41cf11d140b79d3dcca77
checkmarxAutoQA <65957156+checkmarxAutoQA@users.noreply.github.com>
2020-09-29 02:36:23 -0700

    Update 2222.java

--- 2222.java
+++ 2222.java
@@ -9,6 +9,7 @@ public class JdbcSelectTest extends ScalaJdbcConnectSelect {   // Save as "JdbcS
 	   //rgjhfklrfjghkjrhkjg
 	   //fkbdhskjfhkjbdef
 	   //thkjtgkljhlktjhkl
+	   //dfjaegjhdgasjhfgjdhasf
 	   InputStreamReader in = new InputStreamReader(System.in);
 	    InputStreamReader in1 = new InputStreamReader(System.in);
        BufferedReader br = new BufferedReader(in);
